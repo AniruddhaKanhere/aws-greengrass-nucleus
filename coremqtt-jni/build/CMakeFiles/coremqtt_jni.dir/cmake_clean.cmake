75d97e2aabd43116a43a0c14361e9b66bd3b1f92
AniruddhaKanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2026-05-13 17:09:10 -0700

    feat: replace aws-c-mqtt with coreMQTT via channel handler adapter
    - Add coremqtt-jni/ native adapter that plugs coreMQTT into the
      aws-c-io channel pipeline, replacing aws-c-mqtt as the MQTT
      protocol handler while keeping TLS/sockets/event loops intact.
    - Add CoreMqttJniClient implementing IndividualMqttClient interface.
    - Selectable via mqtt.version: "coremqtt" in device config.
    - coreMQTT added as git submodule under coremqtt-jni/coreMQTT.
    - Exclude coremqtt-jni/ from license header check.

---
+++ coremqtt-jni/build/CMakeFiles/coremqtt_jni.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt.c.o"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt.c.o.d"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt_prop_deserializer.c.o"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt_prop_deserializer.c.o.d"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt_prop_serializer.c.o"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt_prop_serializer.c.o.d"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt_serializer.c.o"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt_serializer.c.o.d"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt_serializer_private.c.o"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt_serializer_private.c.o.d"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt_state.c.o"
+  "CMakeFiles/coremqtt_jni.dir/coreMQTT/source/core_mqtt_state.c.o.d"
+  "CMakeFiles/coremqtt_jni.dir/src/coremqtt_channel_handler.c.o"
+  "CMakeFiles/coremqtt_jni.dir/src/coremqtt_channel_handler.c.o.d"
+  "CMakeFiles/coremqtt_jni.dir/src/coremqtt_jni.c.o"
+  "CMakeFiles/coremqtt_jni.dir/src/coremqtt_jni.c.o.d"
+  "libcoremqtt_jni.dylib"
+  "libcoremqtt_jni.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/coremqtt_jni.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
